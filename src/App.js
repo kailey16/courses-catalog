20ef68241b58d538a59334708d85bf1f819e0a71
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2020-02-19 22:06:00 -0500

    route added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import SearchAndFilters from './components/SearchAndFilters'
 import CoursesContainer from './components/CoursesContainer'
-import { Route } from 'react-router-dom';
+import CourseDetail from './components/CourseDetail'
 
 
 class App extends React.Component {
   state = {
     selectedCourse: {}
   }
-  
+
   render() {
     return (
       <div className="App">
