b66fe6ac0f815f47e72f8e07fccb664918d418fa
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2020-02-20 12:06:38 -0500

    home text added

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,11 @@ import CoursesContainer from './CoursesContainer'
 const Home = () => {
   return (
     <div id="Home">
+      <div id="homeText">
+        <h3>Welcome to Medstar courses!</h3>
+        <h4>Please search courses by the keyword or use filters to browse different categories.</h4>
+      </div>
+      
       <Searchbar />
       <Filters />
       <CoursesContainer />
